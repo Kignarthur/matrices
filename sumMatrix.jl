9adad80e13cde267898ce21f19c9231f34e05eb3
Arthur Teixeira <amarthur@usp.br>
2020-06-09 12:24:09 -0300

    Add tests for matrix sum function

--- sumMatrix.jl
+++ sumMatrix.jl
@@ -1,1 +1,13 @@
 # Returns the sum of all elements on the matrix
+using Test
+
+function checkSum()
+    @test matrixSum([]) == 0
+    @test matrixSum([100]) == 100
+    @test matrixSum([1 2;3 4]) == 10
+    @test matrixSum([1 2;-1 -2]) == 0
+    @test matrixSum([1 2 3; 1 2 3]) == 12
+    println("End")
+end
+
+checkSum()
